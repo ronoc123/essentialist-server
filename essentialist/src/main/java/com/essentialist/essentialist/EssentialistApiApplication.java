49a9613552df881d3cdff2f653aa9334af1e52a0
ronoc123 <98913680+ronoc123@users.noreply.github.com>
2024-08-09 22:26:23 -0500

    initial commit of srpiong server

---
+++ essentialist/src/main/java/com/essentialist/essentialist/EssentialistApiApplication.java
@@ -0,0 +1,13 @@
+package com.essentialist.essentialist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssentialistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssentialistApiApplication.class, args);
+	}
+
+}
---
+++ essentialist/src/test/java/com/essentialist/essentialist/EssentialistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essentialist.essentialist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssentialistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
